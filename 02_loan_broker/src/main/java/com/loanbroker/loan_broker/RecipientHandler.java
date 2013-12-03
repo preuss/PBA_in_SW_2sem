7b13108d063d20a0469c986aa69f415aa0685aec
Preuss <Preuss@10.18.132.245>
2013-12-03 14:02:29 +0100

    Upgraded to Java version 1.7 Updated RecepientHandler

--- 02_loan_broker/src/main/java/com/loanbroker/loan_broker/RecipientHandler.java
+++ 02_loan_broker/src/main/java/com/loanbroker/loan_broker/RecipientHandler.java
@@ -5,6 +5,9 @@ import com.loanbroker.loan_broker.models.BankDTO;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.ConsumerCancelledException;
+import com.rabbitmq.client.QueueingConsumer;
+import com.rabbitmq.client.ShutdownSignalException;
 import java.io.BufferedOutputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -21,6 +24,8 @@ import org.simpleframework.xml.core.Persister;
  * @author Marc
  */
 public class RecipientHandler extends Thread {
+
+	private boolean pleaseStop = false;
 
 	private final String QUEUE_NAME;
 	private final String QUEUE_NAME_BANK_1;
@@ -45,11 +50,72 @@ public class RecipientHandler extends Thread {
 	public void run() {
 		try {
 			Connection conn = getConnection();
-			
+			Channel chan = conn.createChannel();
+			chan.queueDeclare(QUEUE_NAME, false, false, false, null);
+			System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+			QueueingConsumer consumer = new QueueingConsumer(chan);
+			chan.basicConsume(QUEUE_NAME, true, consumer);
+			//start polling messages
+			while (pleaseStop == false) {
+				String consumerTag = consumer.getConsumerTag();
+				System.out.println(" [-] ConsumerTag: '" + consumerTag + "'");
+				QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+				String xmlStr = delivery.getBody().toString();
+				processBankOutput(xmlStr);
+				//CanonicalDTO dto = new String(delivery.getBody());
+				//System.out.println(" [x] Received '" + message + "'");
+			}
 		} catch (IOException ex) {
 			Logger.getLogger(RecipientHandler.class.getName()).log(Level.SEVERE, null, ex);
+		} catch (InterruptedException ex) {
+			Logger.getLogger(RecipientHandler.class.getName()).log(Level.SEVERE, null, ex);
+		} catch (ShutdownSignalException ex) {
+			Logger.getLogger(RecipientHandler.class.getName()).log(Level.SEVERE, null, ex);
+		} catch (ConsumerCancelledException ex) {
+			Logger.getLogger(RecipientHandler.class.getName()).log(Level.SEVERE, null, ex);
 		}
+	}
 
+	private void processBankOutput(String xmlStr) {
+		Serializer serializer = new Persister();
+		try {
+			CanonicalDTO dto = serializer.read(CanonicalDTO.class, xmlStr);
+
+			Connection conn = getConnection();
+			for (BankDTO bank : dto.getBanks()) {
+				Channel channel = conn.createChannel();
+
+				String quequeName = getBankChannelName(bank.getName());
+				if (quequeName != null) {
+					channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+					channel.basicPublish("", QUEUE_NAME, null, xmlStr.getBytes());
+					System.out.println(" [x] Sent '" + xmlStr + "'");
+				} else {
+					// TODO: publish to error queue.
+				}
+			}
+		} catch (Exception ex) {
+			Logger.getLogger(RecipientHandler.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+
+	private String getBankChannelName(String bankName) {
+		switch (bankName) {
+			case "xml":
+				return QUEUE_NAME_BANK_1;
+			case "json":
+				return QUEUE_NAME_BANK_2;
+			case "rabbitmq":
+				return QUEUE_NAME_BANK_3;
+			case "webservice":
+				return QUEUE_NAME_BANK_4;
+			default:
+				return null;
+		}
+	}
+
+	public void pleaseStop() {
+		pleaseStop = true;
 	}
 
 	private Connection getConnection() throws IOException {
