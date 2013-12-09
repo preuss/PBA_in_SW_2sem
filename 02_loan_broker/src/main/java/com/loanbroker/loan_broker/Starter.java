4d2bfc84fe24c9b0122653f89ac20b2e17d8cec6
Preuss <Preuss@10.18.133.15>
2013-12-09 11:03:40 +0100

    Upgraded to Java version 1.7 Updated RecepientHandler

---
+++ 02_loan_broker/src/main/java/com/loanbroker/loan_broker/Starter.java
@@ -0,0 +1,11 @@
+package com.loanbroker.loan_broker;
+
+/**
+ * @author Preuss
+ */
+public class Starter {
+	public static void main(String[] args) {
+		BankHandler bankHandler = new BankHandler();
+	}
+	
+}
